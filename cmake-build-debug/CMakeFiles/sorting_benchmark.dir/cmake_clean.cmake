df27adc0ba5c576756080c75540a608067796b16
Tiberiu Popescu <tiberiu1204@gmail.com>
2024-03-27 23:39:19 +0200

    Added MIN_ARRAY_SIZE macro for better controll on array size.

---
+++ cmake-build-debug/CMakeFiles/sorting_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sorting_benchmark.dir/main.cpp.obj"
+  "CMakeFiles/sorting_benchmark.dir/main.cpp.obj.d"
+  "libsorting_benchmark.dll.a"
+  "sorting_benchmark.exe"
+  "sorting_benchmark.exe.manifest"
+  "sorting_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sorting_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
